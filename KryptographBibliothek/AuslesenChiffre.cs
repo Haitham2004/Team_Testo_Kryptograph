3ca0fa9c60b1722e464c8648a4fa21cee9a456d7
Tobias Cremer <tobias.c11@gso.schule.koeln>
2022-05-06 12:12:27 +0200

    Methode mit Main verknüpft

--- KryptographBibliothek/AuslesenChiffre.cs
+++ KryptographBibliothek/AuslesenChiffre.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using Figgle;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace KryptographBibliothek
 {
     public class AuslesenChiffre
     {
+        public static void Auslesenchiffre()
+        {
+            Console.WriteLine
+                    (FiggleFonts.Slant.Render("Team Testo"));
+
+            Console.Title = "Team Testo";
+
+
+            Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                              "                              >>> Chiffre auslesen <<<\n" +
+                              "------------------------------------------------------------------------------------\n\n");
+
+
+
+
+
+        }
+
 
 
 
