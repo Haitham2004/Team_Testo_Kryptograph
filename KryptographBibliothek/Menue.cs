fba363c6b7a7008ecc1495e068cb1737cb3b1088
Haitham Kazzaz <haitham.kazzaz@gmail.com>
2022-05-06 23:11:28 +0200

    Menü teilweise erstellt

--- KryptographBibliothek/Menue.cs
+++ KryptographBibliothek/Menue.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Figgle;
 
 namespace KryptographBibliothek
 {
@@ -10,13 +9,76 @@ namespace KryptographBibliothek
             string HauptAusw;
             bool Exit = false;
             bool Hmenue;
-            do 
+            do
             {
                 (int, int) cPosBM = Console.GetCursorPosition();
                 Console.WriteLine();
-                Console.WriteLine
-                    (FiggleFonts.Slant.Render("Kryptographie"));
-            }
+
+                Console.Title = "BFTMultiTool";
+
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Informatik <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+                Console.WriteLine("Eingabe: exit\t\t->\tbeendet das Program");
+                Console.WriteLine("\n\n Hier Können Sie Verschlüssene Texte entschlüssen");
+                Console.WriteLine("Eingabe: hauptmenü\t->\tzurück zum Hauptmenü");
+
+                Hmenue = false;
+
+                 
+                Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
+                Console.WriteLine("\t1 - Auslesen Chiffre\n");
+                Console.WriteLine("\t2 - Auslesen Tabelle\n ");
+                Console.WriteLine("\t3 - Zeichen Zaehlen\n");
+                Console.WriteLine("\t4 - Zeichen Entfernen\n");
+                Console.WriteLine("\t5 - Zeichen Ersetzen \n");
+                Console.WriteLine("\t6 - Ergebnis Ausgabe\n");
+                Console.Write("Eingabe:");
+                HauptAusw = Console.ReadLine();
+
+                switch (HauptAusw)
+                {
+
+                    case "1":
+                        Console.Clear();
+                        AuslesenChiffre();
+
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Feauture10.Feature10Funcion1();
+
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        Feature11.Feature11Funcion1();
+
+                        break;
+
+                    case "4":
+                        Console.Clear();
+                        Feature20.Feature20Funcion1();
+
+                        break;
+
+                    case "5":
+                        Console.Clear();
+                        Feature21.Feature21Funcion1();
+
+                        break;
+
+                    case "hauptmenü":
+                        Console.Clear();
+                        Hmenue = true;
+                        break;
+                    case "exit":
+                        Exit = true;
+                        break;
+
+
+                } while (!Exit);
         }
     }
 }
