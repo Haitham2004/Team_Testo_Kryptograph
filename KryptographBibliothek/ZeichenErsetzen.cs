be0f527cb7dbef200d9651e348caf0f8f32f8f08
Jowan Hamko <jowan.h2@gso.schule.koeln>
2022-05-11 12:12:51 +0200

    Mein beispiel fertig

--- KryptographBibliothek/ZeichenErsetzen.cs
+++ KryptographBibliothek/ZeichenErsetzen.cs
@@ -11,6 +11,10 @@ namespace KryptographBibliothek
 
             //K 1/7 
             //L 3/7
+            //Y 1/7
+            //U 1/7
+            //B 1/7
+
 
             Dictionary<string, double> tabelle_zeichen_deutsch = new Dictionary<string, double>();
 
@@ -21,13 +25,13 @@ namespace KryptographBibliothek
             tabelle_zeichen_deutsch.Add("B", 0.0196);
             tabelle_zeichen_deutsch.Add("C", 0.0316);
             tabelle_zeichen_deutsch.Add("D", 0.0498);
-            tabelle_zeichen_deutsch.Add("E", 0.1693);
-            tabelle_zeichen_deutsch.Add("F", 0.0149);
+            tabelle_zeichen_deutsch.Add("E", 0.1693);    // 0.14 = U 
+            tabelle_zeichen_deutsch.Add("F", 0.0149);    // 0.14 = Y 
             tabelle_zeichen_deutsch.Add("G", 0.0302);
-            tabelle_zeichen_deutsch.Add("H", 0.0498);
+            tabelle_zeichen_deutsch.Add("H", 0.0498);    // 0.43 = L 
             tabelle_zeichen_deutsch.Add("I", 0.0802);
             tabelle_zeichen_deutsch.Add("J", 0.0024);
-            tabelle_zeichen_deutsch.Add("K", 0.0132);
+            tabelle_zeichen_deutsch.Add("K", 0.0132);    // 0.14 = K 
             tabelle_zeichen_deutsch.Add("L", 0.0360);
             tabelle_zeichen_deutsch.Add("M", 0.0255);
             tabelle_zeichen_deutsch.Add("N", 0.1053);
@@ -45,16 +49,16 @@ namespace KryptographBibliothek
             tabelle_zeichen_deutsch.Add("W", 0.0178);
             tabelle_zeichen_deutsch.Add("X", 0.0005);
             tabelle_zeichen_deutsch.Add("Y", 0.0005);
-            tabelle_zeichen_deutsch.Add("Z", 0.0121);
+            tabelle_zeichen_deutsch.Add("Z", 0.0121);    // 0.14 = B 
 
             Dictionary<string, double> tabelle_zeichen_englisch = new Dictionary<string, double>();
             Console.WriteLine("Englich Wahrschenilichkeiten buchstaben");
 
             tabelle_zeichen_englisch.Add("A", 0.0834);
-            tabelle_zeichen_englisch.Add("B", 0.0154);
+            tabelle_zeichen_englisch.Add("B", 0.0154);  // 0.14 = K 
             tabelle_zeichen_englisch.Add("C", 0.0273);
             tabelle_zeichen_englisch.Add("D", 0.0414);
-            tabelle_zeichen_englisch.Add("E", 0.1260);
+            tabelle_zeichen_englisch.Add("E", 0.1260);  // 0.14 = U 
             tabelle_zeichen_englisch.Add("F", 0.0203);
             tabelle_zeichen_englisch.Add("G", 0.0192);
             tabelle_zeichen_englisch.Add("H", 0.0611);
@@ -65,14 +69,14 @@ namespace KryptographBibliothek
             tabelle_zeichen_englisch.Add("M", 0.0253);
             tabelle_zeichen_englisch.Add("N", 0.0680);
             tabelle_zeichen_englisch.Add("O", 0.0770);
-            tabelle_zeichen_englisch.Add("P", 0.0166);
+            tabelle_zeichen_englisch.Add("P", 0.0166);  // 0.14 = Y 
             tabelle_zeichen_englisch.Add("Q", 0.0009);
-            tabelle_zeichen_englisch.Add("R", 0.0568);
+            tabelle_zeichen_englisch.Add("R", 0.0568);  // 0.43 = L 
             tabelle_zeichen_englisch.Add("S", 0.0611);
             tabelle_zeichen_englisch.Add("T", 0.0937);
             tabelle_zeichen_englisch.Add("U", 0.0285);
             tabelle_zeichen_englisch.Add("V", 0.0106);
-            tabelle_zeichen_englisch.Add("W", 0.0234);
+            tabelle_zeichen_englisch.Add("W", 0.0234); // 0.14 = B
             tabelle_zeichen_englisch.Add("X", 0.0020);
             tabelle_zeichen_englisch.Add("Y", 0.0204);
             tabelle_zeichen_englisch.Add("Z", 0.0006);
@@ -81,11 +85,14 @@ namespace KryptographBibliothek
 
             tabelle_zeichen_chiffre.Add("K", 0.14);
             tabelle_zeichen_chiffre.Add("L", 0.43);
+            tabelle_zeichen_chiffre.Add("Y", 0.14);
+            tabelle_zeichen_chiffre.Add("U", 0.14);
+            tabelle_zeichen_chiffre.Add("B", 0.14);
+
+            // klartext auf Deutsch = Khf ez
+            // Klartext auf Englisch = Ber by
 
         }
 
-
-            
-    } 
-    
+    }
 }
