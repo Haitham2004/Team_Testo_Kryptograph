7d9bd664242d96025fc260a76b2e6b0ef04b8712
Tobias Cremer <tobias.c11@gso.schule.koeln>
2022-05-11 12:51:00 +0200

    Auslesen Chiffre fast fertig

--- KryptographBibliothek/AuslesenChiffre.cs
+++ KryptographBibliothek/AuslesenChiffre.cs
@@ -4,7 +4,20 @@ namespace KryptographBibliothek
 {
     public class AuslesenChiffre
     {
+        public static void Auslesenchiffre()
+        {
 
+            string pfad = @"C:\Users\tobias.c11\source\repos\Team_Testo_Kryptograph\Chiffre.txt";
+
+
+            string text = System.IO.File.ReadAllText(pfad);
+            System.Console.WriteLine("Ausgabe von Chiffre.txt = \n {0}", text);
+
+
+          
+
+
+        }
 
 
     }
